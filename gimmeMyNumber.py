3ecfcecac27d789e984fa2f85a008b0aca92ab13
Richard Ortiz <ortizrichard2@gmail.com>
2021-04-26 16:23:19 -0400

    added gimme my number html and py version and happy new year html version

---
+++ gimmeMyNumber.py
@@ -0,0 +1,9 @@
+﻿userNum = input("gimme a number greater than 100: ")
+changedNum = int(userNum)
+
+while (changedNum < 100):
+    print(userNum + " is less than 100, try again")
+    userNum = input("gimme a number: ")
+    changedNum = int(userNum)
+
+print(userNum + " is greater than 100. Nice")
