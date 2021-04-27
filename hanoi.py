59cd2e978d3eaa1046d368cc43bd7c6b0a355f9f
Richard Ortiz <ortizrichard2@gmail.com>
2021-04-26 21:48:31 -0400

    completed both versions of whichfloor and hanoi py file

---
+++ hanoi.py
@@ -0,0 +1,19 @@
+﻿def move(f,t):
+    print("move disk from {} to {}!".format(f,t))
+
+#move("A","C")
+
+def moveVia(f,v,t):
+    move(f,v)
+    move(v,t)
+
+def hanoi(n,f,h,t):
+    if(n==0):
+        pass
+    else:
+        hanoi(n-1,f,t,h)
+        move(f,t)
+        hanoi(n-1,h,f,t)
+
+hanoi(8,"A","B","C")
+    
---
+++ whichfloor.py
@@ -0,0 +1,10 @@
+﻿maximum_stories = 10
+userNum = input("Enter the number of the floor your office is on: ")
+changedNum = int(userNum)
+
+while (changedNum > maximum_stories):
+    print("You entered " + userNum + ". There are 10 stories in the building. Please enter a valid number.")
+    userNum = input("Enter the number of the floor your office is on: ")
+    changedNum = int(userNum)
+
+print("Great job, your office is on floor: " + userNum)
